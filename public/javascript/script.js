ff30be625a639f1fc3343772321d85cc98ea11ac
Vlad974-web <73641487+Vlad974-web@users.noreply.github.com>
2021-06-09 09:42:07 +0200

    respnsive

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -5,3 +5,21 @@ window.addEventListener("scroll", function(){
   // DONNER LA NOUVELLE CLASE POUR HEADER
   header.classList.toggle('sticky', window.scrollY > 0);
 })
+
+
+// JavaScript du menu de la barre latérale de navigation réactif
+const menuBtn = document.querySelector(".menu-btn");
+const nav = document.querySelector(".nav");
+const navigationItems = document.querySelectorAll('.nav a');
+
+menuBtn.addEventListener("click", () => {
+  menuBtn.classList.toggle("active");
+  nav.classList.toggle("active");
+})
+
+navigationItems.forEach((navigationItems) => {
+  navigationItems.addEventListener('click', () => {
+    menuBtn.classList.remove("active");
+    nav.classList.remove("active");
+  })
+})
