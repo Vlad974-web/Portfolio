35b09b77997dbd5bc6b10141f05a8d066ab43e9e
Vlad974-web <73641487+Vlad974-web@users.noreply.github.com>
2021-06-21 16:23:27 +0200

    Requêtes préparer

--- controller/indexController.js
+++ controller/indexController.js
@@ -1,8 +1,8 @@
 // AFFICHER LA LISTE DES ARTICLES
 exports.getIndexController = async (req, res) => {
   // BASE DE DONNE
-  const projet = await querysql(
-    'SELECT titre, image, description, gitURL, projetID FROM projets')
+  const projet = await querysql('SELECT ??, ??, ??, ??, ?? FROM projets',
+  ["titre", "image", "description", "gitURL", "projetID"])
 
   // AFFICER LA PAGE index.ejs
   res.render('index', {projets: projet});
