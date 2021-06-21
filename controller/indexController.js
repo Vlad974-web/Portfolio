df47ce7d715f69759614f32b05d2ca6de974f9c2
Vlad974-web <73641487+Vlad974-web@users.noreply.github.com>
2021-06-21 14:11:53 +0200

    Base de donnée
    Afficher sur la page bdd

--- controller/indexController.js
+++ controller/indexController.js
@@ -1,5 +1,9 @@
 // AFFICHER LA LISTE DES ARTICLES
 exports.getIndexController = async (req, res) => {
+  // BASE DE DONNE
+  const projet = await querysql(
+    'SELECT titre, image, description, gitURL, projetID FROM projets')
+
   // AFFICER LA PAGE index.ejs
-  res.render('index');
+  res.render('index', {projets: projet});
 }
