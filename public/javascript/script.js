4cc8b4e85a250e69f30dbb28fc989e3e7016c5c4
Vlad974-web <73641487+Vlad974-web@users.noreply.github.com>
2021-06-21 10:28:34 +0200

    Finished projet html, css

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -23,3 +23,36 @@ navigationItems.forEach((navigationItems) => {
     nav.classList.remove("active");
   })
 })
+
+
+// JAVASCRIPT POUR LE BOUTON DE DEFILEMENT VERS LE HAUT
+const scrollBtn = document.querySelector('.scrollToTop-btn');
+
+window.addEventListener("scroll", function () {
+  scrollBtn.classList.toggle("active", window.scrollY > 500);
+})
+
+
+// JAVASCRIPT POUR FAIRE DEFILER VERS LE HAUT EN CLIQUANT SUR LE BUTTON DE DEFILEMENT VERS LE HAUT
+scrollBtn.addEventListener('click', () => {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+})
+
+
+//JAVASCRIPT POUR RÉVÉLER LES ÉLÉMENTS DU SITE WEB SUR SCROLL
+window.addEventListener('scroll', reveal);
+
+function reveal() {
+  var reveals = document.querySelectorAll('.reveal');
+
+  for(var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var revealTop = reveals[i].getBoundingClientRect().top;
+    var revealPoint = 50;
+
+    if(revealTop < windowHeight - revealPoint) {
+      reveals[i].classList.add('active');
+    }
+  }
+}
