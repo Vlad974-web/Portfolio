71cca7a70b8034b1357d65178c263e19de7df8cb
Vlad974-web <73641487+Vlad974-web@users.noreply.github.com>
2021-07-05 18:53:22 +0200

    Créé le controller pour la page admin

---
+++ controller/admin/adminController.js
@@ -0,0 +1,7 @@
+//======================================================= PAGE DE CONNEXION
+// GET
+exports.getInscriptionPage = async (req, res) => {
+
+  /* AFFICHER LA PAGE, inscription.ejs */
+  res.render('inscription')
+}
